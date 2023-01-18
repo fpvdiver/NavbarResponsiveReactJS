8f57b933cd8df0dc45172ffdbb15011e9526b76a
Allan Augusto <54289072+fpvdiver@users.noreply.github.com>
2023-01-18 18:03:00 -0300

    Initial commit

---
+++ src/components/MenuData.js
@@ -0,0 +1,44 @@
+export const MenuData = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user"
+  },
+
+  {
+    title: "Cursos Recreativos",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user"
+  },
+  {
+    title: "Cursos Técnicos",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user"
+  },
+  {
+    title: "Turismo",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user"
+  },
+  {
+    title: "GoPro",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user"
+  },
+  {
+    title: "Contato",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user"
+  },
+  {
+    title: "Entrar",
+    url: "#",
+    cName: "nav-links-mobile"
+  }
+];
